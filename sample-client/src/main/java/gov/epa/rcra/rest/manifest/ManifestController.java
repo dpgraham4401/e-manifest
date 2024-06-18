770d2ae602898cb85e270ca0f267ad78741591fe
David Paul Graham <dpgraham4401@gmail.com>
2024-06-18 13:22:01 -0400

    initial controller to uploading multipart form data

--- sample-client/src/main/java/gov/epa/rcra/rest/manifest/ManifestController.java
+++ sample-client/src/main/java/gov/epa/rcra/rest/manifest/ManifestController.java
@@ -4,6 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/manifest")
@@ -18,6 +25,18 @@ public class ManifestController {
     @GetMapping(value = "/{manifestTrackingNumber}", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getManifest(@PathVariable String manifestTrackingNumber) {
         return manifestService.getEmanifest(manifestTrackingNumber);
+    }
+
+    @PostMapping(value = "", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> createManifest(@RequestPart("file") MultipartFile file,
+                                                 @RequestPart("json") String data) throws IOException {
+        if (file.isEmpty()) {
+            return new ResponseEntity<>("Error", HttpStatus.BAD_REQUEST);
+        }
+        Path destination =
+                Paths.get("local").resolve(Objects.requireNonNull(file.getOriginalFilename())).normalize().toAbsolutePath();
+        Files.copy(file.getInputStream(), destination);
+        return new ResponseEntity<>("Created", HttpStatus.CREATED);
     }
 
     @ExceptionHandler(ManifestException.class)
